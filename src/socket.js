692e38d907d8efd18a7a58ea4270b674a44ad9c2
SeptianTm <septian@think-match.com>
2023-12-28 11:01:53 +0700

    change .env

--- src/socket.js
+++ src/socket.js
@@ -3,7 +3,7 @@ import { io } from 'socket.io-client';
 // "undefined" means the URL will be computed from the `window.location` object
 
 // https://be-collector.thinkmatch.id
-const URL = import.meta.env.VITE_LINK_BACKEND
+const URL = 'https://be-collector.thinkmatch.id'
 
 
 export const socket = io(URL);
