140f4d2c2160abe5fb1bdd63d9568a173d3c9cb3
Teknomise <teknomise.indo@gmail.com>
2024-02-19 11:53:28 +0700

    update

--- models/db.js
+++ models/db.js
@@ -1,6 +1,7 @@
 import Dexie from 'dexie';
 
 export const db = new Dexie('myDatabase');
+
 db.version(3).stores({
   activity:'++id,activityname,delaydescription,kode',
   operator:'++id,jde,fullname,position',
