9276c5ba0d68ec38bf3f3a4b5fe7f0804fb9bf5a
Teknomise <teknomise.indo@gmail.com>
2024-03-08 16:42:20 +0700

    update

--- src/socket.js
+++ src/socket.js
@@ -1,7 +1,7 @@
 import { io } from 'socket.io-client';
 
-// const URL = 'https://be-collector.thinkmatch.id'
-const URL = 'http://127.0.0.1:3939'
+const URL = 'https://be-collector.thinkmatch.id'
+// const URL = 'http://127.0.0.1:3939'
 
 const initializedSocket = () => {
     const socket = io(URL);
