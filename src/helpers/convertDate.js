7a19444a5126388504a19d3179a3404ab73c4d84
Teknomise <teknomise.indo@gmail.com>
2024-03-26 14:45:30 +0700

    update

--- src/helpers/convertDate.js
+++ src/helpers/convertDate.js
@@ -41,3 +41,21 @@ export function convertDateFormat(inputDate) {
   return `${day}-${month}-${year}`;
 }
 
+export function convertDateFormatTime(inputDate) {
+  // Parse the input date string
+  const date = new Date(inputDate);
+
+  // Extract date components
+  const day = String(date.getDate()).padStart(2, '0');
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const year = date.getFullYear();
+  const hours = String(date.getHours()).padStart(2, '0');
+  const minutes = String(date.getMinutes()).padStart(2, '0');
+  const seconds = String(date.getSeconds()).padStart(2, '0');
+
+  // Format the date string
+  const formattedDate = `${day}-${month}-${year} ${hours}:${minutes}:${seconds}`;
+
+  return formattedDate;
+}
+
