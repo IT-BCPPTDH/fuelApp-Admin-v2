19a27f15d64b56be21d9967ddc88555eed28c2b5
Teknomise <teknomise.indo@gmail.com>
2024-03-29 16:41:02 +0700

    Update with monitoring brekdown

--- src/services/timeEntry.js
+++ src/services/timeEntry.js
@@ -1,17 +1,17 @@
 import axios from 'axios'
-import Cookies from 'js-cookie'
+// import Cookies from 'js-cookie'
 import { URL_ENUMS } from '../utils/Enums'
 
 const api = axios.create({
   maxBodyLength: Infinity
 })
 
-api.interceptors.request.use(config => {
-  const jwt = Cookies.get('token')
-  if (!jwt) throw new Error('JWT not found in cookies')
-  config.headers.Authorization = `Bearer ${jwt}`
-  return config
-})
+// api.interceptors.request.use(config => {
+//   const jwt = Cookies.get('token')
+//   if (!jwt) throw new Error('JWT not found in cookies')
+//   config.headers.Authorization = `Bearer ${jwt}`
+//   return config
+// })
 
 const getMasterActivity = async () => {
   const response = await api.get(URL_ENUMS.masterActivity)
